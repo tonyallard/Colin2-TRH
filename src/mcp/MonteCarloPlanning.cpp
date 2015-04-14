cbdf1bcfbedeb73bb9314677dba3ec6c559524fa
Tony <tony.g.allard@gmail.com>
2015-04-14 10:26:56 +0930

    Partially Pseudocoded MCPlanning and UCT

--- src/mcp/MonteCarloPlanning.cpp
+++ src/mcp/MonteCarloPlanning.cpp
@@ -19,6 +19,21 @@ int MonteCarloPlanning::MonteCarloPlanning(const Planner::MinimalState & state) 
 int MonteCarloPlanning::search (const Planner::MinimalState & state, int depth) {
 	if (isTerminal(state)) return 0;
 	if (isLeaf(state)) return evaluate(state);
-
+	ActionSegment action = selectAction(state, depth);
+	TransitionResult result = simulateAction(state, action);
+	int q = result.getReward() + (gamma * search(result.getNextState(), depth + 1);
+	updateValue(state, action, q, depth);
+	return q;
 }
+
+const TransitionResult & MonteCarloPlanning::simulateAction(const Planner::MinimalState & state, Planner::ActionSegment & action) {
+	//Globals::remainingActionsInPlan - might need this to determine available actions
+	//Will need something to get TILs
+
+	//Get set of valid actions
+	//Determine action selection via UCT
+	//Generate TransitionResult and return
+}
+
+
 
--- src/mcp/MonteCarloPlanning.h
+++ src/mcp/MonteCarloPlanning.h
@@ -1,5 +1,5 @@
-#ifndef __MONTECARLOPLANNING
-#define __MONTECARLOPLANNING
+#ifndef MCP__MONTECARLOPLANNING_H_
+#define MCP__MONTECARLOPLANNING_H_
 
 namespace MCPlanning
 {
@@ -7,6 +7,9 @@ namespace MCPlanning
 	
 	private:
 		static int search(const Planner::MinimalState & state, int depth);
+		static const Planner::ActionSegment & selectAction(const Planner::MinimalState & state, int depth);
+		static const TransitionResult & simulateAction(const Planner::MinimalState & state, Planner::ActionSegment & action);
+		static double gamma = 0.1;
 	
 	public:
 		static int MonteCarloPlanning(const Planner::MinimalState & state);
